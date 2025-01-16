4b0316bc1585e80ad2b64a4035c4503ae2190edd
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-01-16 09:22:30 +0500

    implement layout structure with Header and Footer components, integrate Redux for authentication state management

--- Mega_Project-Blog_app/src/App.jsx
+++ Mega_Project-Blog_app/src/App.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { useState } from "react";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import authService from "./appwrite/auth";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default App
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    authService
+      .getCurrentAccount()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <>
+      <div className="min-h-screen flex flex-wrap content-between bg-gray-300">
+        <div className="w-full block">
+          <Header />
+
+          <main>
+            <Outlet />
+          </main>
+
+          <Footer />
+        </div>
+      </div>
+    </>
+  ) : null;
+};
+
+export default App;
---
+++ Mega_Project-Blog_app/src/components/index.js
@@ -0,0 +1,5 @@
+import Footer from "./Footer/Footer.jsx";
+import Header from "./Header/Header.jsx";
+
+
+export {Footer, Header};
