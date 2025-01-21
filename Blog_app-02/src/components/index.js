5761ad7523b49f15c74d964cc034319604c9ec7d
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-01-21 14:49:22 +0500

    feat: add reusable components including Logo, Button, Input, and Container; enhance Header with navigation and logout functionality

--- Blog_app-02/src/components/index.js
+++ Blog_app-02/src/components/index.js
@@ -1,4 +1,8 @@
+import Container from "./container/Container.jsx";
 import Footer from "./Footer/Footer.jsx";
 import Header from "./Header/Header.jsx";
+import Logo from "./Logo.jsx";
+import Button from "./Button.jsx";
+import Input from "./Input.jsx";
 
-export { Footer, Header };
+export { Input, Button, Container, Footer, Header, Logo };
