a9372e600f602cf6ccb136437aafdc5a3cd0f9a3
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-01-29 12:27:31 +0500

    feat: migrate to Speech Recognition App with Vite and Tailwind CSS; remove old context API structure

---
+++ Speech Recognition App/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import useClipboard from "react-use-clipboard";
+
+const App = () => {
+  const [copyText, setCopyText] = useState();
+  const [isCopied, setIsCopied] = useClipboard(copyText, {
+    successDuration: 10000,
+  });
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  const stopListening = () => SpeechRecognition.stopListening();
+
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
+  return (
+    <div className="p-10 items-center drop-shadow-lg shadow-lg rounded-2xl m-1 w-auto">
+      <h1 className="text-4xl text-center">Speech Recognition App</h1>
+      <div className="w-10 h-20" onClick={() => setCopyText(transcript)}>
+        {transcript}
+      </div>
+
+      <div className="flex p-10 gap-5">
+        <button
+          className="cursor-pointer bg-blue-400 p-2 py-1 rounded-md"
+          onClick={setIsCopied}
+        >
+          {isCopied ? "Copied" : "Copy to Clipboard"}
+        </button>
+        <button
+          className="bg-blue-400 p-2 py-1 rounded-md cursor-pointer"
+          onClick={startListening}
+        >
+          Start Listening
+        </button>
+        <button
+          className="bg-blue-400 p-2 py-1 rounded-md cursor-pointer"
+          onClick={stopListening}
+        >
+          Stop Listening
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
